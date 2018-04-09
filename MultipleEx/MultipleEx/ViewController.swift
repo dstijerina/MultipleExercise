6a144f015730bd5d34b4125e36af2312859fe951
dstijerina <37883240+dstijerina@users.noreply.github.com>
2018-04-09 16:28:37 -0500

    Add files via upload

---
+++ MultipleEx/MultipleEx/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  MultipleEx
+//
+//  Created by Tijerina, Daniel on 4/9/18.
+//  Copyright © 2018 Tijerina, Daniel. All rights reserved.
+//
+
+import UIKit
+
+class Global {
+    var nameVar = String()
+    var switchVar = String()
+    var sliderVar:Int = 0
+}
+
+let global = Global()
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theName: UILabel!
+    
+    
+    @IBOutlet weak var theSwitch: UILabel!
+    
+    
+    @IBOutlet weak var theSlider: UILabel!
+    
+    
+    @IBOutlet weak var myName: UITextField!
+    
+    
+    @IBAction func myButton(_ sender: Any) {
+        global.nameVar = myName.text!
+        theName.text = "Name: " + global.nameVar
+        myName.resignFirstResponder()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool){
+        theSwitch.text = "Switch: \(global.switchVar)"
+        theSlider.text = "Slider: \(global.sliderVar)"
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
